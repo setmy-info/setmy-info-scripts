155c07481bc1af61ae41f4d3b30a455d11aa2270
Imre Tabur <imre.tabur@eesti.ee>
2021-06-20 22:34:26 +0300

    term command suports gnome terminal now

---
+++ resources/lib/konsole.sh
@@ -0,0 +1,18 @@
+function findTerminal() {
+    if ! command -v gnome-terminal &> /dev/null
+    then
+        echo "Gnome terminal could not be found"
+    else
+        gnome-terminal --working-directory=${PWD} &
+        exit 0
+    fi
+
+    if ! command -v konsole &> /dev/null
+    then
+        echo "KDE knosole could not be found"
+        exit 1
+    else
+        konsole --workdir ${PWD} &
+        exit 0
+    fi
+}

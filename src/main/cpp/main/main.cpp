2e7680206bcf672647b7e577d06c8fb5cb0f4c1a
Imre Tabur <imre.tabur@eesti.ee>
2023-06-26 07:05:42 +0300

    Build cleaned from Cpp build

--- src/main/cpp/main/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Application.h"
-
-using namespace SetMyInfo;
-
-int main(int argc, char *argv[], char *env[]) {
-    Application app;
-    return app.Args(argc, argv, env);
-}
--- src/test/cpp/library/include/CppUnitTest.h
+++
@@ -1,6 +0,0 @@
-#ifndef SET_MY_INFO_CPP_UNIT_TEST_H
-#define	SET_MY_INFO_CPP_UNIT_TEST_H
-
-#include <boost/test/included/unit_test.hpp>
-
-#endif // SET_MY_INFO_CPP_UNIT_TEST_H

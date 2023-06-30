4da9bedc51efb0767b60d2700a3ea7fe7b0d6174
Imre Tabur <imre.tabur@eesti.ee>
2023-06-30 10:50:09 +0300

    If systemd is missing, then systemd scrit is not created

--- src/main/sh/rpm/post.install.sh
+++ src/main/sh/rpm/post.install.sh
@@ -10,7 +10,9 @@ fi
 
 SMI_PROVIDER=setmy.info
 ln -f -s /opt/${SMI_PROVIDER}/etc/profile.d/setmy-info.sh /etc/profile.d/setmy-info.sh
-ln -f -s /opt/${SMI_PROVIDER}/etc/systemd/system/example.service /etc/systemd/system/example.service
+if command -v systemctl >/dev/null 2>&1; then
+    ln -f -s /opt/${SMI_PROVIDER}/etc/systemd/system/example.service /etc/systemd/system/example.service
+fi
 ln -f -s /opt/${SMI_PROVIDER}/bin/smi-binary /opt/${SMI_PROVIDER}/bin/smi-test
 ln -f -s /opt/${SMI_PROVIDER}/bin/smi-binary /opt/${SMI_PROVIDER}/bin/smi-stealer
 

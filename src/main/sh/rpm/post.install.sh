0cb3304a474d250ce2495efff51a1fbb13e2a5ec
Imre Tabur <imre.tabur@eesti.ee>
2023-06-30 10:39:17 +0300

    User adding on demand

--- src/main/sh/rpm/post.install.sh
+++ src/main/sh/rpm/post.install.sh
@@ -2,7 +2,12 @@
 
 # Executed by installer at install step end.
 echo "### Post-Install"
-useradd --system microservice
+
+username=microservice
+if ! id "$username" >/dev/null 2>&1; then
+    useradd --system $username
+fi
+
 SMI_PROVIDER=setmy.info
 ln -f -s /opt/${SMI_PROVIDER}/etc/profile.d/setmy-info.sh /etc/profile.d/setmy-info.sh
 ln -f -s /opt/${SMI_PROVIDER}/etc/systemd/system/example.service /etc/systemd/system/example.service

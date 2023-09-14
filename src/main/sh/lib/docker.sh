3e1e2137aca6229fa820dc290abf08315c4c3889
Imre Tabur <imre.tabur@eesti.ee>
2023-09-14 12:12:58 +0300

    Refactored Packaging vol2

--- src/main/sh/lib/docker.sh
+++ src/main/sh/lib/docker.sh
@@ -12,6 +12,7 @@ dockerBuild() {
 }
 
 build_image() {
+    echo "Building image ${DOCKER_ID_ORGANIZATION}/${DOCKER_PROJECT_NAME}:${DOCKER_PROJECT_VERSION}"
     export BUILDKIT_PROGRESS=plain
     export DOCKER_BUILDKIT=0
     docker build -t ${DOCKER_ID_ORGANIZATION}/${DOCKER_PROJECT_NAME}:${DOCKER_PROJECT_VERSION} .
@@ -22,18 +23,22 @@ build_image() {
 }
 
 build_latest_tag() {
+    echo "Setting latest tag"
     docker image tag ${DOCKER_ID_ORGANIZATION}/${DOCKER_PROJECT_NAME}:${DOCKER_PROJECT_VERSION} ${DOCKER_ID_ORGANIZATION}/${DOCKER_PROJECT_NAME}:latest
 }
 
 build_show_images() {
+    echo "Getting image list"
     docker image list
 }
 
 build_login() {
+    echo "Doing Docker Hub login"
     docker login
 }
 
 build_push() {
+    echo "Start pushing image"
     docker image push ${DOCKER_ID_ORGANIZATION}/${DOCKER_PROJECT_NAME}:latest
     docker image push ${DOCKER_ID_ORGANIZATION}/${DOCKER_PROJECT_NAME}:${DOCKER_PROJECT_VERSION}
 }

8dd4ba6c9797422eafc1435e6d24285e48d638d0
Imre Tabur <imre.tabur@mail.ee>
2023-11-27 12:59:01 +0200

    Lineending changed. Small fix for installation.

--- src/main/resources/cmake/sh/base/etc.cmake
+++ src/main/resources/cmake/sh/base/etc.cmake
@@ -6,7 +6,7 @@ IF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     INSTALL(FILES "${ETC_OUTPUT_PATH}/environment.conf"               DESTINATION etc)
     INSTALL(FILES "${ETC_OUTPUT_PATH}/systemd/system/example.service" DESTINATION etc/systemd/system)
     INSTALL(FILES "${ETC_OUTPUT_PATH}/yum.repos.d/kubernetes.repo"    DESTINATION etc/yum.repos.d)
-    INSTALL(FILES "${ETC_OUTPUT_PATH}/yum.repos.d/nginx.repo"         DESTINATION etc/nginx.repos.d)
+    INSTALL(FILES "${ETC_OUTPUT_PATH}/yum.repos.d/nginx.repo"         DESTINATION etc/yum.repos.d)
 ENDIF()
 
 # PLACEHOLDER-END #

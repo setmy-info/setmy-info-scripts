5891a8766f82dc689b08134770521fcf8b3e84ca
Imre Tabur <imre.tabur@mail.ee>
2025-06-04 20:32:29 +0300

    mvn jdk gradle downloads

--- incubation/seleniumVersionChecker.groovy
+++ incubation/seleniumVersionChecker.groovy
@@ -49,10 +49,14 @@ interface DriverExecute {
     void execute(WebDriver driver)
 }
 
+interface Url {
+    String getUrl()
+}
+
 class CliArgs {
 
     @Option(names = ["--name"], description = "Package name", required = false)
-    String packageName
+    List<String> packageNames
 
     CommandLine commandLine
 
@@ -135,37 +139,67 @@ class RulesRegister {
 
 abstract class DriverExecuteBase {
     static final List<String> packageExtensions = [
-            ".tar", ".tar.gz", ".tgz", ".tar.bz2", ".tbz2", ".tar.xz", ".txz", ".tar.Z",
-            ".zip", ".gz", ".bz2", ".xz", ".7z",
-            ".deb", ".rpm",
-            ".run", ".sh", ".bin",
-            ".appimage",
-            ".jar",
-            ".dmg",
-            ".exe"
+        ".tar", ".tar.gz", ".tgz", ".tar.bz2", ".tbz2", ".tar.xz", ".txz", ".tar.Z",
+        ".zip", ".gz", ".bz2", ".xz", ".7z",
+        ".deb", ".rpm",
+        ".run", ".sh", ".bin",
+        ".appimage",
+        ".jar",
+        ".dmg",
+        ".exe"
     ]
+
+    By tagName(String name) {
+        By.tagName(name)
+    }
+
+    By id(String id) {
+        By.id(id)
+    }
+
+    By className(String id) {
+        By.className(id)
+    }
+
+    By cssSelector(String id) {
+        By.cssSelector(id)
+    }
+
+    By xpath(String id) {
+        By.xpath(id)
+    }
+
+    List<String> packageHrefs(List<String> links) {
+        def hrefs = links.collect { it.getAttribute("href") }.findAll { it != null }
+        def filteredHrefs = hrefs.findAll { href ->
+            def hrefLowerCase = href.toLowerCase()
+            packageExtensions.any { ext ->
+                hrefLowerCase.endsWith(ext)
+            }
+        }
+        return filteredHrefs
+    }
+
+    String getVersion(String url) {
+        def versionPattern = ~/(?<![a-zA-Z0-9])(\d+\.\d+\.\d+)(?![a-zA-Z0-9])/
+        //def versionPattern = ~/(\d+\.\d+\.\d+)/
+        def matcher = (url =~ versionPattern)
+        if (matcher.find()) {
+            def version = matcher.group(1)
+            version
+        }
+        ""
+    }
 }
 
-class MavenDriverExecute extends DriverExecuteBase implements DriverExecute, Name {
+class MavenDriverExecute extends DriverExecuteBase implements DriverExecute, Name, Url {
 
     @Override
     void execute(WebDriver driver) {
-
         try {
-            def url = "https://maven.apache.org/download.cgi"
-            driver.get(url)
-            //println "✅ Page loaded: ${url}"
-
-            def links = driver.findElements(By.tagName("a"))
-            def hrefs = links.collect { it.getAttribute("href") }.findAll { it != null }
-
-            def filteredHrefs = hrefs.findAll { href ->
-                def hrefLowerCase = href.toLowerCase()
-                packageExtensions.any { ext ->
-                    hrefLowerCase.endsWith(ext)
-                }
-            }
-            //println "🔗 Found links:"
+            driver.get(getUrl())
+            def links = driver.findElements(tagName("a"))
+            def filteredHrefs = packageHrefs(links)
             //filteredHrefs.each { println it }
             def finalFiltered = filteredHrefs.findAll { href ->
                 href = href.toLowerCase()
@@ -175,22 +209,16 @@ class MavenDriverExecute extends DriverExecuteBase implements DriverExecute, Nam
 
                 return true
             }
-
-            //println "🔗 Version and package extension links:"
             //finalFiltered.each { println it }
-
             def sortedByUrl = finalFiltered.sort { a, b -> a <=> b }
-            //println "🔗 Sorted links:"
             //sortedByUrl.each { println it }
-
             def lastItem = sortedByUrl.last()
             def versionPattern = ~/(?<![a-zA-Z0-9])(\d+\.\d+\.\d+)(?![a-zA-Z0-9])/
             def matcher = (lastItem =~ versionPattern)
             if (matcher.find()) {
                 def version = matcher.group(1)
-                println "${version}"
+                //println "${version}"
             }
-
             println "${lastItem}"
         } catch (Exception e) {
             println "❌ Error: ${e.message}"
@@ -198,8 +226,101 @@ class MavenDriverExecute extends DriverExecuteBase implements DriverExecute, Nam
     }
 
     @Override
+    String getUrl() {
+        return "https://maven.apache.org/download.cgi"
+    }
+
+    @Override
     String getName() {
         return "mvn"
+    }
+}
+
+class JdkDriverExecute extends DriverExecuteBase implements DriverExecute, Name, Url {
+
+    @Override
+    void execute(WebDriver driver) {
+        try {
+            driver.get(getUrl())
+            String firstHref = "https://jdk.java.net/"
+            def links = driver.findElements(tagName("a"))
+            def hrefs = links
+                .collect { it.getAttribute("href") }
+                .findAll { it != null }
+                .findAll { it.toLowerCase().startsWith(firstHref) && it.length() > firstHref.length() }
+            //https://jdk.java.net/24
+            //Begins with: https://jdk.java.net/ and is longer than that
+            def jdkDownloadPageUrl = hrefs.first()
+            //println "jdkDownloadPageUrl ${jdkDownloadPageUrl}"
+            driver.get(jdkDownloadPageUrl)
+            links = driver.findElements(tagName("a"))
+            //https://download.java.net/java/GA/jdk24.0.1/24a58e0e276943138bf3e963e6291ac2/9/GPL/openjdk-24.0.1_linux-x64_bin.tar.gz
+            def filteredHrefs = packageHrefs(links)
+            //filteredHrefs.each { println it }
+
+            def finalFiltered = filteredHrefs.findAll { href ->
+                href = href.toLowerCase()
+                if (!href.endsWith(".tar.gz")) return false
+                if (!href.contains("linux")) return false
+                if (!href.contains("x64")) return false
+                return true
+            }
+            finalFiltered.each { println it }
+        } catch (Exception e) {
+            println "❌ Error: ${e.message}"
+        }
+    }
+
+    @Override
+    String getUrl() {
+        return "https://openjdk.org/"
+    }
+
+    @Override
+    String getName() {
+        return "jdk"
+    }
+}
+
+class GradleDriverExecute extends DriverExecuteBase implements DriverExecute, Name, Url {
+
+    @Override
+    void execute(WebDriver driver) {
+        try {
+            driver.get(getUrl())
+            def links = driver.findElements(tagName("a"))
+            def hrefs = links
+                .collect { it.getAttribute("href") }
+                .findAll { it != null }
+
+            def finalFiltered = hrefs.findAll { href ->
+                href = href.toLowerCase()
+                if (!href.endsWith("&format=bin")) return false
+                return true
+            }
+
+            //https://gradle.org/next-steps/?version=8.14.1&format=bin
+            def gradleDownloadPageUrl = finalFiltered.first()
+            def version = gradleDownloadPageUrl
+                .replace("https://gradle.org/next-steps/?version=", "")
+                .replace("&format=bin", "")
+            //println gradleDownloadPageUrl
+            //println version
+            def downloadUrl = "https://services.gradle.org/distributions/gradle-${version}-bin.zip"
+            println downloadUrl
+        } catch (Exception e) {
+            println "❌ Error: ${e.message}"
+        }
+    }
+
+    @Override
+    String getUrl() {
+        return "https://gradle.org/install/"
+    }
+
+    @Override
+    String getName() {
+        return "gradle"
     }
 }
 
@@ -213,13 +334,14 @@ static void main(String[] args) {
     def yaml = new Yaml()
     def config = yaml.load(new File(cliArgs.configFile).text)
     */
-
     try {
         geckoDriver.init()
         firefox.init()
         //println "Package name: ${cliArgs.getPackageName()}"
-        def rule = requireNonNull(rulesRegister[cliArgs.getPackageName()], "❌ Missing rule: '${cliArgs.getPackageName()}'")
-        rule.execute(firefox.getDriver())
+        cliArgs.getPackageNames().each {
+            def rule = requireNonNull(rulesRegister[it], "❌ Missing rule: '${it}'")
+            rule.execute(firefox.getDriver())
+        }
     } catch (Exception exception) {
         println "❌ Error: ${exception.message}"
     } finally {
@@ -228,7 +350,13 @@ static void main(String[] args) {
 }
 
 static RulesRegister fillWithRules(RulesRegister rulesRegister) {
-    def driverExecute = new MavenDriverExecute()
+    fillWithRules(new MavenDriverExecute(), rulesRegister)
+    fillWithRules(new JdkDriverExecute(), rulesRegister)
+    fillWithRules(new GradleDriverExecute(), rulesRegister)
+    return rulesRegister
+}
+
+static RulesRegister fillWithRules(DriverExecute driverExecute, RulesRegister rulesRegister) {
     rulesRegister[(driverExecute as Name).getName()] = driverExecute
     return rulesRegister
 }

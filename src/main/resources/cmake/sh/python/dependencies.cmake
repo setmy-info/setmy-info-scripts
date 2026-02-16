39cf991a04b1150f923852e78dad322abcff329a
Imre Tabur <imre.tabur@mail.ee>
2026-02-16 21:03:03 +0200

    Python man pages

--- src/main/resources/cmake/sh/python/dependencies.cmake
+++ src/main/resources/cmake/sh/python/dependencies.cmake
@@ -2,7 +2,8 @@
 MESSAGE("-- python dependencies.cmake")
 
 ADD_DEPENDENCIES(buildPythonShellScripts makeDirectories)
+ADD_DEPENDENCIES(buildPythonMan          makeDirectories)
 
-ADD_DEPENDENCIES(build buildPythonShellScripts)
+ADD_DEPENDENCIES(build buildPythonShellScripts buildPythonMan)
 
 # PLACEHOLDER-END #
--- src/main/resources/cmake/sh/python/targets.cmake
+++ src/main/resources/cmake/sh/python/targets.cmake
@@ -2,5 +2,6 @@
 MESSAGE("-- python targets.cmake")
 
 ADD_CUSTOM_TARGET(buildPythonShellScripts cp ${PYTHON_SH_SOURCES_BIN_PATH}/* ${BINARY_OUTPUT_PATH})
+ADD_CUSTOM_TARGET(buildPythonMan          cp ${MAIN_MAN_SOURCES_PATH}/python/man1/*.1 ${MAN_OUTPUT_PATH}/man1 && gzip -f ${MAN_OUTPUT_PATH}/man1/*.1)
 
 # PLACEHOLDER-END #

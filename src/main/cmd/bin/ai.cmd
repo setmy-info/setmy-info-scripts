38017bdbddbc1e00f16d9cf5787224aaabd72bc9
Imre Tabur <imre.tabur@mail.ee>
2026-02-25 07:49:13 +0200

    Fox for Windows AI scripts

--- src/main/cmd/bin/ai.cmd
+++ src/main/cmd/bin/ai.cmd
@@ -1,11 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Try to get SMI_LIB_DIR via smi-lib-location
-set SMI_LIB_DIR=
-for /f "delims=" %%i in ('smi-lib-location.cmd 2^>nul') do set SMI_LIB_DIR=%%i
-if "!SMI_LIB_DIR!"=="" set SMI_LIB_DIR=C:\pub\setmy.info\lib
-set AI_LIB_DIR=!SMI_LIB_DIR!\profiles\ai
+set PATH_BACKUP=%PATH%
+set PUB_DIR=C:\pub
+set SMI_HOME=%PUB_DIR%\setmy.info
+set SMI_BIN_DIR=%SMI_HOME%\bin
+set SMI_LIB_DIR=%SMI_HOME%\lib
+set TERM_PROFILES_DIR=%SMI_LIB_DIR%\profiles
+set AI_PROFILES_DIR=%SMI_LIB_DIR%\ai
 
 for %%p in (%*) do (
     call :process_arg %%p
@@ -14,18 +16,13 @@ goto :eof
 
 :process_arg
 set ARG=%1
-set AI_FILE=!AI_LIB_DIR!\!ARG!.md
-set GROUP_FILE=!AI_LIB_DIR!\!ARG!.group
+set AI_FILE=!AI_PROFILES_DIR!\!ARG!.md
 
 if exist "!AI_FILE!" (
     for /f "usebackq tokens=*" %%l in ("!AI_FILE!") do (
         if not "%%l"=="" echo - %%l
     )
-) else if exist "!GROUP_FILE!" (
-    for /f "usebackq tokens=*" %%g in ("!GROUP_FILE!") do (
-        if not "%%g"=="" call :process_arg %%g
-    )
 ) else (
-    echo Profile or group not found: !ARG! >&2
+    echo Profile not found: !ARG! >&2
 )
 goto :eof

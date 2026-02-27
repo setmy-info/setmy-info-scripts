013ab5f1b37cbe06cdd3010ba4c75463332208b0
Imre Tabur <imre.tabur@mail.ee>
2026-02-27 21:21:42 +0200

    Taklist preprarations

--- src/main/sh/build/build-tasklist.sh
+++ src/main/sh/build/build-tasklist.sh
@@ -2,8 +2,12 @@
 
 # ./src/main/sh/build/build-tasklist.sh in root folder
 
-cat AGENTS-INTRO.md > TASKLIST.md \
-&& ai setmy-info-scripts cmake groovy git cleancode bottom-up >> TASKLIST.md \
-&& cat TASKLIST-CONTENT.md >> TASKLIST.md
+cat src/main/resources/tasklist/AGENTS-INTRO.md > TASKLIST.md
+ai setmy-info-scripts cmake groovy git cleancode bottom-up >> TASKLIST.md
+echo "" >> TASKLIST.md
+echo "## Tasklist" >> TASKLIST.md
+echo "" >> TASKLIST.md
+# TODO: parameter instad TASKLIST-CONTENT.md
+cat TASKLIST-CONTENT.md >> TASKLIST.md
 
 exit 0

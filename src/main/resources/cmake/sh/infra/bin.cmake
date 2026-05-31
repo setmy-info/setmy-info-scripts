da810e4c9cf9fab12953fe8ec71cdd6f5b7a76a8
Imre Tabur <imre.tabur@mail.ee>
2026-05-31 10:04:34 +0300

    K8S and Argo locations

--- src/main/resources/cmake/sh/infra/bin.cmake
+++ src/main/resources/cmake/sh/infra/bin.cmake
@@ -1,4 +1,14 @@
 # PLACEHOLDER-BEGIN #
 MESSAGE("-- infra bin.cmake")
 
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-k8s-location"                   DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-argo-location"                  DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-k8s-pvc-name"                   DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-workflow-workspace-location"    DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-workflow-ai-workspace-location" DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-packages-shared-location"       DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-zfs-org-create"                 DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-k8s-secret-set"                 DESTINATION bin)
+INSTALL(PROGRAMS "${BINARY_OUTPUT_PATH}/smi-k8s-configmap-set"              DESTINATION bin)
+
 # PLACEHOLDER-END #
--- src/main/resources/cmake/sh/infra/dependencies.cmake
+++ src/main/resources/cmake/sh/infra/dependencies.cmake
@@ -1,4 +1,9 @@
 # PLACEHOLDER-BEGIN #
 MESSAGE("-- infra dependencies.cmake")
 
+ADD_DEPENDENCIES(build          buildK8sYaml buildArgoYaml buildInfraMan)
+ADD_DEPENDENCIES(buildK8sYaml   makeDirectories)
+ADD_DEPENDENCIES(buildArgoYaml  makeDirectories)
+ADD_DEPENDENCIES(buildInfraMan  makeDirectories buildMan)
+
 # PLACEHOLDER-END #

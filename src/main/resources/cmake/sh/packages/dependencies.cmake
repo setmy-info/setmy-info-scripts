96b43ecae494655f340ef168859f1290996d4540
Imre Tabur <imre.tabur@mail.ee>
2026-08-06 20:07:31 +0300

    Fixed man page for spider

--- src/main/resources/cmake/sh/packages/dependencies.cmake
+++ src/main/resources/cmake/sh/packages/dependencies.cmake
@@ -1,9 +1,10 @@
 # PLACEHOLDER-BEGIN #
 MESSAGE("-- packages dependencies.cmake")
 
-ADD_DEPENDENCIES(build buildPackages buildPackagesLib buildPackagesBin)
+ADD_DEPENDENCIES(build buildPackages buildPackagesLib buildPackagesBin buildPackagesMan)
 ADD_DEPENDENCIES(buildPackages    makeDirectories)
 ADD_DEPENDENCIES(buildPackagesLib makeDirectories)
 ADD_DEPENDENCIES(buildPackagesBin makeDirectories)
+ADD_DEPENDENCIES(buildPackagesMan makeDirectories)
 
 # PLACEHOLDER-END #
--- src/main/resources/cmake/sh/packages/targets.cmake
+++ src/main/resources/cmake/sh/packages/targets.cmake
@@ -4,6 +4,6 @@ MESSAGE("-- packages targets.cmake")
 ADD_CUSTOM_TARGET(buildPackages    cp -R ${MAIN_SH_SOURCES_PATH}/packages/lib/packages/* ${LIBRARY_OUTPUT_PATH}/packages)
 ADD_CUSTOM_TARGET(buildPackagesBin cp -R ${MAIN_SH_SOURCES_PATH}/packages/bin/* ${BINARY_OUTPUT_PATH})
 ADD_CUSTOM_TARGET(buildPackagesLib cp ${MAIN_SH_SOURCES_PATH}/packages/lib/*.sh ${LIBRARY_OUTPUT_PATH})
-#ADD_CUSTOM_TARGET(buildPackagesMan cp ${PACKAGES_MAN_SOURCES_PATH}/man1/*.1 ${MAN_OUTPUT_PATH}/man1 && gzip -f ${MAN_OUTPUT_PATH}/man1/*.1)
+ADD_CUSTOM_TARGET(buildPackagesMan cp ${PACKAGES_MAN_SOURCES_PATH}/man1/*.1 ${MAN_OUTPUT_PATH}/man1 && gzip -f ${MAN_OUTPUT_PATH}/man1/*.1)
 
 # PLACEHOLDER-END #

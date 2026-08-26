32be272022a9ebdcb53a75fff047d5bc4118f01c
Imre Tabur <imre.tabur@mail.ee>
2026-08-26 10:47:24 +0300

    More advanced hello world in Elixir

--- src/main/elixir/hello/lib/hello-elixir.exs
+++ src/main/elixir/hello/lib/hello-elixir.exs
@@ -1,1 +1,13 @@
-IO.puts("Hello, world!")
+exit_code =
+  case System.argv() do
+    [] -> 0
+    [code | _] -> String.to_integer(code)
+  end
+
+IO.puts("Hello, world from #{File.cwd!()}!")
+
+if exit_code != 0 do
+  IO.puts("Exiting with code #{exit_code}.")
+end
+
+System.halt(exit_code)

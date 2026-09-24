6d5ee56930a5de1816350df373e73ef83b188906
Imre Tabur <imre.tabur@mail.ee>
2026-09-24 19:43:36 +0300

    Example config

--- src/main/resources/cmake/sh/server/etc.cmake
+++ src/main/resources/cmake/sh/server/etc.cmake
@@ -1,6 +1,6 @@
 # PLACEHOLDER-BEGIN #
 MESSAGE("-- server etc.cmake")
 
-INSTALL(FILES "${ETC_OUTPUT_PATH}/smi-serve-files.config" DESTINATION etc)
+INSTALL(FILES "${ETC_OUTPUT_PATH}/smi-serve-files.config.example" DESTINATION etc)
 
 # PLACEHOLDER-END #
